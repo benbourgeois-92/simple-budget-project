8d1699ce87d59608ff505554fd4754745b626220
Ben Bourgeois <1stbourgeois@gmail.com>
2021-04-13 15:01:48 -0400

    Add Expense nearing completion

--- client/src/operations/modalScreens.js
+++ client/src/operations/modalScreens.js
@@ -1,7 +1,11 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import GlobalContext from '../user-context';
 import currencyFormat from './conversions';
+import Flickity from 'react-flickity-component';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 import '../css/modal_screens.css';
+import '../css/form-slide.css'
 
 export const DeleteExpenseScreen = (props) => {
     
@@ -61,21 +65,68 @@ export const UpdateTransactionScreen = (props) => {
 export const AddExpenseScreen = (props) => {
 
     const {togglePopup, operation, screen} = useContext(GlobalContext);
+    const[dateState, setDateState] = useState(null)
+    const [newExpense, setNewExpense] = useState({
+        id: null,
+        title: '',
+        amount: 0,
+        amountSaved: 0,
+        dueDate: null,
+        moneyIn: 'Payday/No Automatic Funding',
+        contribution: 'Reach Target Balance/Set Aside Target Amount',
+        moneyOut: 'No Automatic Spending',
+        recentTransactions: [],
+        checked: false
+    })
 
-    const order = {type: 'ADD_EXPENSE',item: null}
+    const {title, amount, dueDate, checked} = newExpense;
+    
+    const onChange = (e) => {
+        setNewExpense({ ...newExpense, [e.target.name]: e.target.value });
+    }
 
+
+    const order = {type: 'ADD_EXPENSE', item: null}
+    const flickityOptions ={ cellAlign: 'left',wrapAround: false,groupCells: 1,contain: true,prevNextButtons: true,pageDots: true}
+    
     return (
+            
             <div>
-                <h2>Add New Expense</h2>
-                <div>
-                    <label htmlFor="expenseName">Expense Title:</label>
-                    <input type="text" id="expenseName" placeholder="enter your expense title here"/>							
-                </div>
-                <div>
-                    <label htmlFor="expenseName">Expense Name:</label>
-                    <input type="text" id="expenseName" placeholder="Enter Expense Name"/>							
-                </div>
+                <h2 className="centerText noselect">Add New Expense</h2>
+                <p className="centerText noselect">Swipe screen, click arrows or tab to proceed.</p>
 
+            <div className="formSlider">
+                <Flickity options={flickityOptions}>
+                    <div className="formSlide">
+                        <label htmlFor="expenseName">Expense Title:</label>
+                        <input type="text" id="expenseName" placeholder="enter your expense title here"/>							
+                    </div>
+                    <div className="formSlide">
+                        <div className="horizontal">
+                            <label htmlFor="amount">Amount:</label>
+                            <input type="text" id="amount" placeholder="your monthly amount" required pattern="[0-9]"/>						
+                        </div>
+                        <p className="centerText instructions"><span>Tip:</span> Enter the amount you need monthly.</p>
+
+                    </div>
+                    <div className="formSlide">
+                        <DayPicker onDayClick={(day) => setNewExpense({dueDate: day})} selectedDays={dueDate} />
+                            {dueDate ? (<p className="centerText instructions">You selected {dueDate.toLocaleDateString()}</p>) 
+                            : (<p className="centerText instructions">Enter the date you need {currencyFormat(amount)} by.</p>)}
+                    </div>
+                    <div className="formSlide">
+                        <p className="centerText bold">How do you want money to be moved to this expense?</p>
+                        <div className="selectOptions">
+                            <ul>
+                        
+                                <li><button className="checkboxButton">On Payday<input  type="checkbox" checked={checked} value="On Payday"/></button></li>
+                                <li><button className="checkboxButton">No Automatic Funding<input  type="checkbox"  checked={checked} value="On Payday"/></button></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </Flickity>                
+            </div>
                 <ul>
                     <li><button onClick={()=> operation(order)}>Confirm</button></li>
                     <li><button onClick={()=> togglePopup(screen.popupOpen)} className="closeMenu">Cancel</button></li>                    
